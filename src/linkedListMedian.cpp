8c68a40bfe6f122fd8d3670cc917df835cb25aa0
BhargavaRavaliKoganti <BhargavaRavaliKoganti@missionrnd.com>
2016-01-22 22:48:22 +0530

    Submit Commit

--- src/linkedListMedian.cpp
+++ src/linkedListMedian.cpp
@@ -20,5 +20,17 @@ struct node {
 };
 
 int linkedListMedian(struct node *head) {
+	if (head != NULL){
+		struct node *ptr = head;
+		struct node *cur = head;
+		while (cur != NULL && cur->next != NULL){
+			if (cur->next->next == NULL){
+				return (ptr->num + ptr->next->num) / 2;
+			}
+			cur = cur->next->next;
+			ptr = ptr->next;
+		}
+		return ptr->num;
+	}
 	return -1;
 }
